cb8e18e98dfc9a8e0df438d52b1311b93f787cb4
Abhinav Ghatak <155328157+AbhinavGhatak@users.noreply.github.com>
2026-07-13 12:01:21 +0530

    Time: 16 ms (39.48%) | Memory: 47.6 MB (48.59%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,31 +1,26 @@
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
 
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i : nums) {
+        for (int i : nums) {
             map.put(i, map.getOrDefault(i, 0) + 1);
         }
         
-        /* CHANGED HERE: Defined and populated minHeap because it was used below without declaration. 
-           Removed the 'if(entry.getValue() >= k)' loop as it caused logical errors. */
         PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> map.get(a) - map.get(b));
-        for (int num : map.keySet()) {
-            minHeap.add(num);
+        for (int key : map.keySet()) {
+            minHeap.add(key);
             if (minHeap.size() > k) {
                 minHeap.poll();
             }
         }
         
-        /* CHANGED HERE: Renamed the array from 'result' to 'ans' to fix the duplicate variable definition error */
-        int[] ans = new int[k];
+        int[] result = new int[k];
         for (int i = 0; i < k; i++) {
-            ans[i] = minHeap.poll();
+            result[i] = minHeap.poll();
         }
-        return ans;
+        return result;
     }
 }
