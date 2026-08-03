e9e1d14d79fc8a5057b04629c08d153131c04893
Abhinav Ghatak <155328157+AbhinavGhatak@users.noreply.github.com>
2026-08-03 19:15:31 +0530

    Time: 0 ms (100.00%) | Memory: 48 MB (57.77%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,9 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if (n==1) {
+            return x;
+        }
+        double product = Math.pow(x, n);
+        return product;
+    }
+}
