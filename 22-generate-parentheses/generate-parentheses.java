9171add88be22793f3537a64e402acbdb60bb780
Abhinav Ghatak <155328157+AbhinavGhatak@users.noreply.github.com>
2026-08-07 13:21:15 +0530

    Time: 7 ms (5.00%) | Memory: 52.2 MB (5.03%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,14 +1,12 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-        List<String> res = new ArrayList<>();
-        if (n == 0) {
+        List <String> res = new ArrayList<>();
+        if (n==0) {
             res.add("");
-            return res;
         }
-
-        for (int i = 0; i < n; i++) {
+        for (int i =0; i< n; i++) {
             for (String left : generateParenthesis(i)) {
-                for (String right : generateParenthesis(n - 1 - i)) {
+                for (String right : generateParenthesis(n-1-i)) {
                     res.add("(" + left + ")" + right);
                 }
             }
